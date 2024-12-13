48aac994d6f7c8b205d681b4d2073f636257489b
mehkij <disismehki@gmail.com>
2024-12-13 14:32:45 -0500

    Create new component: Nav

---
+++ src/components/Nav.tsx
@@ -0,0 +1,32 @@
+import { NavigationMenu, NavigationMenuList, NavigationMenuItem, NavigationMenuLink, navigationMenuTriggerStyle } from '../components/ui/navigation-menu'
+import { Link } from 'react-router-dom'
+
+type NavLinkProps = {
+    name: string
+    linkTo: string
+}
+
+function Nav() {
+    return (
+        <NavigationMenu>
+        <NavigationMenuList>
+            <NavLink name='Home' linkTo='/' />
+            <NavLink name='My Cards' linkTo='/collection' />
+        </NavigationMenuList>
+        </NavigationMenu>
+    )
+}
+
+function NavLink({ name, linkTo }: NavLinkProps) {
+    return (
+        <NavigationMenuItem>
+            <Link to={linkTo}>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    {name}
+                </NavigationMenuLink>
+            </Link>
+        </NavigationMenuItem>
+    )
+}
+
+export default Nav
