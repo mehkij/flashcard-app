0228227dc22a51ad56c074fed948407799293d19
mehkij <disismehki@gmail.com>
2024-12-13 20:05:06 -0500

    Create /new-card route handler

---
+++ cardHandler.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/mehkij/flashcard-app/internal/database"
+)
+
+type Card struct {
+	ID          uuid.UUID `json:"id"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	LastTested  time.Time `json:"last_tested"`
+}
+
+func (cfg *apiConfig) createCardHandler(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Title       string    `json:"title"`
+		Description string    `json:"description"`
+		LastTested  time.Time `json:"last_tested"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Printf("Error decoding params: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	card, err := cfg.queries.CreateCard(r.Context(), database.CreateCardParams{
+		Title:       params.Title,
+		Description: params.Description,
+		LastTested: sql.NullTime{
+			Time: params.LastTested,
+		},
+	})
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Error creating card: %s", err))
+		return
+	}
+
+	respondWithJSON(w, 201, Card{
+		ID:          card.ID,
+		CreatedAt:   card.CreatedAt,
+		UpdatedAt:   card.UpdatedAt,
+		Title:       card.Title,
+		Description: card.Description,
+		LastTested:  card.LastTested.Time,
+	})
+}
