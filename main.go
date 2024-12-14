2326e8f22e729b8f76200475f3e36a1949e96ec1
mehkij <disismehki@gmail.com>
2024-12-13 20:04:34 -0500

    Create new API route: /new-card

--- main.go
+++ main.go
@@ -1,20 +1,44 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
+	"github.com/mehkij/flashcard-app/internal/database"
 )
 
+type apiConfig struct {
+	queries *database.Queries
+}
+
 func main() {
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Printf("Unable to open connection to DB: %s", err)
+	}
+	defer db.Close()
+
+	dbQueries := database.New(db)
+
+	apiCfg := &apiConfig{
+		queries: dbQueries,
+	}
+
 	mux := http.NewServeMux()
+	mux.HandleFunc("POST /api/new-card", apiCfg.createCardHandler)
 
 	server := &http.Server{
-		Addr:    ":8080",
+		Addr:    ":5173",
 		Handler: mux,
 	}
 
-	log.Println("Serving files from . on port: 8080")
+	log.Println("Serving files from . on port: 5173")
 	log.Fatal(server.ListenAndServe())
 }
