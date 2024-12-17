9150f798d0890b3d20d3cc8f40bae9cb46c9aa98
mehkij <disismehki@gmail.com>
2024-12-17 13:43:21 -0500

    Prettier

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,23 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 
-import animatePlugin from "tailwindcss-animate"
+import animatePlugin from "tailwindcss-animate";
 
 export default {
-    darkMode: ["class"],
-    content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  darkMode: ["class"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-  	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
-  	}
+    extend: {
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {},
+    },
   },
   plugins: [animatePlugin],
-}
-
+};
