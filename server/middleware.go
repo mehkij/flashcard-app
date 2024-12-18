d6b061c75f540f742b7e2846f00751f2103e37d6
mehkij <disismehki@gmail.com>
2024-12-17 22:17:05 -0500

    Allow POST, PUT and DELETE in CORS

--- server/middleware.go
+++ server/middleware.go
@@ -6,7 +6,7 @@ func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Set CORS Header
 		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173") // Change to '*' for all origins (not recommended)
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
 		// Handle OPTIONS preflight request
